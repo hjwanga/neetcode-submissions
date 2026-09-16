1c04998c78b6743a43bfce5ddfe0622dd995b0d1
hjwanga <40780658+hjwanga@users.noreply.github.com>
2026-09-16 17:52:54 +0800

    Add: search-for-word - submission-2

---
+++ Data Structures & Algorithms/search-for-word/submission-2.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    static constexpr int dirs[4][2]{{0,1}, {0,-1}, {1,0}, {-1,0}};
+    bool dfs(const vector<vector<char>>& board, const string& word, int r, int c, int index, vector<vector<bool>>& visited) {
+        int m = board.size();
+        int n = board[0].size();
+        if (index == (int)word.size()) {
+            return true;
+        }
+        if (r < 0 || r >= m || c < 0 || c >= n) {
+            return false;
+        }
+        if (word[index] != board[r][c]) {
+            return false;
+        }
+        if (visited[r][c]) {
+            return false;
+        }
+        visited[r][c] = true;
+        for (const auto& dir : dirs) {
+            int new_r = r + dir[0];
+            int new_c = c + dir[1];
+            bool one_sol = dfs(board, word, new_r, new_c, index+1, visited);
+            if (one_sol) {
+                return true;
+            }
+        }
+        visited[r][c] = false;
+        return false;
+    }
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        int m = board.size();
+        int n = board[0].size();
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (dfs(board, word, r, c, 0, visited)) {
+                    return true;
+                }        
+            }
+        }
+        return false;
+    }
+};
+
+// "CAC"
