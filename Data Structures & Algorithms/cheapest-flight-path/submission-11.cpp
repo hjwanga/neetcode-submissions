9c425a84ca3a964b33ab44df16763af8fc105116
hjwanga <40780658+hjwanga@users.noreply.github.com>
2026-09-23 20:28:45 +0800

    Add: cheapest-flight-path - submission-11

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-11.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    using pii = pair<int,int>;
+    using State = tuple<int,int,int>;
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        // movement: k++
+        k++;
+        // [from] = <to, price>
+        vector<vector<pii>> graph(n);
+        for (const auto& flight : flights) {
+            int from = flight[0];
+            int to = flight[1];
+            int price = flight[2];
+            graph[from].emplace_back(to, price);
+        }
+
+        // [node][move] = price
+        vector<vector<int>> min_prices(n, vector<int>(k+1, INT_MAX));
+        // <price, node, move>
+        priority_queue<State, vector<State>, greater<State>> pq;
+        min_prices[src][0] = 0;
+        pq.emplace(min_prices[src][0], src, 0);
+
+        while (!pq.empty()) {
+            auto [price, node, move] = pq.top();
+            pq.pop();
+
+            for (const auto& [next_node, price_curr_to_next] : graph[node]) {
+                int next_move = move+1;
+                if (next_move > k) {
+                    continue;
+                }
+                if (price+price_curr_to_next < min_prices[next_node][next_move]) {
+                    min_prices[next_node][next_move] = price+price_curr_to_next;
+                    pq.emplace(min_prices[next_node][next_move], next_node, next_move);
+                }
+            }
+        }
+
+        int result = INT_MAX;
+        for (int i = 0; i <= k; ++i) {
+            result = min(result, min_prices[dst][i]);
+        }
+        return result == INT_MAX ? -1 : result;
+    }
+};
