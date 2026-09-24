81e7a62a5d797f00c0ee8166a83ce39d5699700a
hjwanga <40780658+hjwanga@users.noreply.github.com>
2026-09-24 14:59:50 +0800

    Add: decode-ways - submission-7

---
+++ Data Structures & Algorithms/decode-ways/submission-7.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    bool is_valid_2_cut(string_view s) {
+        if (s[0] == '0') {
+            return false;
+        }
+        if (s[0] == '1' || (s[0] == '2' && '0' <= s[1] && s[1] <= '6')) {
+            return true;
+        }
+        return false;
+    }
+
+    bool is_valid_1_cut(string_view s) {
+        return s[0] != '0';
+    }
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        string_view sv(s);
+        if (n == 1) {
+            return is_valid_1_cut(sv) ? 1 : 0;
+        }
+        int dp_m2 = 0;
+        int dp_m1 = is_valid_1_cut(sv.substr(0,1)) ? 1 : 0;
+        int dp = 0;
+        for (int i = 1; i < n; ++i) {
+            int sol1 = 0;
+            if (is_valid_1_cut(sv.substr(i,1))) {
+                sol1 = dp_m1;
+            }
+            int sol2 = 0;
+            if (is_valid_2_cut(sv.substr(i-1,2))) {
+                sol2 = i-2 >= 0 ? dp_m2 : 1;
+            }
+            dp = sol1 + sol2;
+            dp_m2 = dp_m1;
+            dp_m1 = dp;
+        }
+        return dp;
+    }
+};
+
+// dp[i] := s[0..i]的numDecodings
+// dp[i] = dp[i-1] + dp[i-2]
+
+//1. 切一個 or 切兩個
+// -> 切一個: [1,9]
+// -> 切兩個: 僅能[10,26]
